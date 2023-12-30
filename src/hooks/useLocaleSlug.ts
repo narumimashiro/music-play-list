67262e80d165e61779b876aa532ec09eff8bb590
NarumiMashiro <narunarumashiro96@gmail.com>
2023-12-30 22:54:32 +0900

    re edit useLocale

--- src/hooks/useLocaleSlug.ts
+++ src/hooks/useLocaleSlug.ts
@@ -8,16 +8,20 @@ import { useRouter } from 'next/router'
 
 export const VALID_LOCALE = ['ja-jp', 'en-us']
 export const I18NEXT_LOCALE = 'i18next_locale'
+export const LOCALE = {
+  JAPANESE: { DEFAULT: 'ja', URL_LOCALE: 'ja-jp'},
+  ENGLISH: { DEFAULT: 'en', URL_LOCALE: 'en-us'}
+}
 
 export const useLocaleSlug = () => {
   const router = useRouter()
   const { locale_slug } = router.query
 
-  return locale_slug as string || 'ja-jp'
+  return locale_slug as string || LOCALE.JAPANESE.URL_LOCALE
 }
 
 export const useDefaultLocale = () => {
-  const [defaultLocale, setDefaultLocale] = useState('ja')
+  const [defaultLocale, setDefaultLocale] = useState(LOCALE.JAPANESE.DEFAULT)
 
   useEffect(() => {
     setDefaultLocale(window.navigator.language)
