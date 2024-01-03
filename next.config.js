039e79b6274b8eaaac1276cb64f5d6f9280737e3
NarumiMashiro <narunarumashiro96@gmail.com>
2024-01-03 13:20:55 +0900

    add debug log function

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const { i18n } = require('./next-i18next.config')
 const nextConfig = {
   i18n,
   compiler: {
-    removeConsole: true
+    removeConsole: process.env.NODE_ENV !== 'development'
   }
 }
 
